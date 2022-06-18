213c9aa911f0e81c6c3dc364f5ed12142b46f98e
Jullliaaaa <107765702+Jullliaaaa@users.noreply.github.com>
2022-06-19 00:46:48 +0800

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int i;
+    int div;
+
+    while( cin >> a )
+	{
+        div = 0;
+		i = 1;
+        while( i <= a )
+			{
+            if( a%i == 0 )
+				{
+                div = div +1;      /****divisible+1***/
+            	}
+            i = i+1;
+        	}
+		if( div == 2 )                /*****2-prime******/
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
+}
