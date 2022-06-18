08bd370df24979bf0135be7ae1196607a2b7de65
Jullliaaaa <107765702+Jullliaaaa@users.noreply.github.com>
2022-06-19 00:53:47 +0800

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>                   // <--------
+using namespace std;
+
+int main()
+{
+    float x;
+
+    while( cin >> x )
+    {
+        cout << sqrt(x) << endl;   // <--------
+    }
+
+    return 0;
+}
