c1ac03dbeaffd418487edb24a8787b6842c56b9f
Jullliaaaa <107765702+Jullliaaaa@users.noreply.github.com>
2022-06-25 00:40:16 +0800

    Add files via upload

---
+++ vector evensum.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include<iostream>
+using namespace std;
+
+int main(){
+vector <int> even={2,4,6,8};
+int sum;
+for (int i=0;i<even.size();i++){
+  sum = sum + even[i];} 
+
+cout<<sum;}
